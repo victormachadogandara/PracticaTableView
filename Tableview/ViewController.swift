cd78f537a1e1630fdd26758c4dcfb3075cfecf03
Alumno <ulsa@d105.local>
2020-11-02 13:24:07 -0700

    Initial Commit

---
+++ Tableview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tableview
+//
+//  Created by Alumno on 02/11/20.
+//  Copyright © 2020 alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
